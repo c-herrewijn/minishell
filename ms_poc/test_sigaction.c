8d831de71973efde7524d6e4609fd3bf65ca4efd
Kevin Kroon <kkroon@student.codam.nl>
2023-05-09 17:36:23 +0200

    seperated pipex poc and minishell poc

---
+++ ms_poc/test_sigaction.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <limits.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv, char **envp)
+{
+
+}
+
