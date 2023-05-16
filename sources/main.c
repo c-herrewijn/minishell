ede037c93e67234dbafd836d6d6c3ef0d70e5605
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-05-16 12:43:26 +0200

    added minishell.h

--- sources/main.c
+++ sources/main.c
@@ -1,24 +1,22 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <limits.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdbool.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/05/16 12:36:57 by cherrewi      #+#    #+#                 */
+/*   Updated: 2023/05/16 12:42:24 by cherrewi      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int argc, char **argv, char **envp)
+#include "minishell.h"
+
+int	main(int argc, char **argv, char **envp)
 {
-	char *str;
+	char	*str;
 
-	while(true)
+	while (true)
 	{
 		str = readline("minishell$ ");
 		if (str == NULL)
@@ -28,10 +26,5 @@ int main(int argc, char **argv, char **envp)
 		// debug
 		printf("%s\n", str);
 	}
-	return 0;
+	return (0);
 }
-
-/*
-cc -lreadline *.c
-./a.out
-*/
