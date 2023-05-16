f5b69086137eb3937b6c387693ae7e3a1f544bad
Kevin Kroon <kkroon@student.codam.nl>
2023-05-16 12:02:13 +0200

    basic readline function with history

--- ms_poc/rl_functions.c
+++ ms_poc/rl_functions.c
@@ -7,7 +7,11 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 // rl functions
 // readline, rl_clear_history, rl_on_new_line,
@@ -15,5 +19,21 @@
 
 int main(int argc, char **argv, char **envp)
 {
+	char *str;
 
+	while(1)
+	{
+		str = readline("input: ");
+		if (str == NULL)
+			break;
+		add_history(str);
+		printf("%s\n", str);
+	}
+	return 0;
 }
+
+/*
+cc -lreadline rl_functions.c -o rl_functions
+./rl_functions
+end with ctrl-c
+*/
