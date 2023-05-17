a9daa676605725cb3ebb8f62e855da66da87e423
Kevin Kroon <kkroon@student.codam.nl>
2023-05-17 22:42:30 +0200

    main update, need to remove/move some functions later

--- sources/main.c
+++ sources/main.c
@@ -6,26 +6,41 @@
 /*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/16 12:36:57 by cherrewi      #+#    #+#                 */
-/*   Updated: 2023/05/17 15:28:38 by kkroon        ########   odam.nl         */
+/*   Updated: 2023/05/17 21:27:46 by kkroon        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void print_2d_array(char **s)
+{
+	int i;
+
+	i = 0;
+	while(s[i] != NULL)
+	{
+		printf("%s\n", s[i]);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	char	*str;
+	t_data data;
+	data.argc = argc;
+	data.argv = argv;
+	data.envp = envp;
 
 	while (true)
 	{
-		str = readline("minishell$ ");
-		if (str == NULL)
+		data.str = readline("minishell$ ");
+		if (data.str == NULL)
 			break ;
-		add_history(str);
+		add_history(data.str);
 		// debug
-		printf("%s\n", str);
+		// printf("%s\n", data.str);
 		
-		check_if_builtin(str, envp);
+		check_if_builtin(data.str, envp);
 	}
 	return (0);
 }
