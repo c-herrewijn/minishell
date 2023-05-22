df30583eef3137cbed72826a4907007a38cdca42
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-05-22 12:29:25 +0200

    changed variable name

--- sources/lexer.c
+++ sources/lexer.c
@@ -6,7 +6,7 @@
 /*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/16 12:44:41 by cherrewi      #+#    #+#                 */
-/*   Updated: 2023/05/17 21:55:53 by cherrewi      ########   odam.nl         */
+/*   Updated: 2023/05/22 12:04:56 by cherrewi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 size_t	count_tokens(char *str)
 {
-	size_t			word_count;
+	size_t			token_count;
 	size_t			i;
 	t_lexer_state	state;
 
 	state = DELIMITED;
-	word_count = 0;
+	token_count = 0;
 	i = 0;
 	while (str[i] != '\0')
 	{
 		if (state == DELIMITED)
-			count_tokens_delimited(&word_count, &state, str, i);
+			count_tokens_delimited(&token_count, &state, str, i);
 		else if (state == READING_OPERATOR)
-			count_tokens_reading_operator(&word_count, &state, str, i);
+			count_tokens_reading_operator(&token_count, &state, str, i);
 		else if (state == READING_WORD)
-			count_tokens_reading_word(&word_count, &state, str, i);
+			count_tokens_reading_word(&token_count, &state, str, i);
 		i++;
 	}
-	return (word_count);
+	return (token_count);
 }
--- sources/lexer_count_tokens.c
+++ sources/lexer_count_tokens.c
@@ -6,13 +6,13 @@
 /*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 21:23:55 by cherrewi      #+#    #+#                 */
-/*   Updated: 2023/05/17 21:55:53 by cherrewi      ########   odam.nl         */
+/*   Updated: 2023/05/22 12:28:32 by cherrewi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	count_tokens_delimited(size_t *word_count, t_lexer_state *state,
+void	count_tokens_delimited(size_t *token_count, t_lexer_state *state,
 	char *str, size_t index)
 {
 	if (is_unquoted_blank(str, index) == true)
@@ -20,16 +20,16 @@ void	count_tokens_delimited(size_t *word_count, t_lexer_state *state,
 	else if (is_unquoted_operator_char(str, index))
 	{
 		*state = READING_OPERATOR;
-		(*word_count)++;
+		(*token_count)++;
 	}
 	else
 	{
 		*state = READING_WORD;
-		(*word_count)++;
+		(*token_count)++;
 	}
 }
 
-void	count_tokens_reading_operator(size_t *word_count, t_lexer_state *state,
+void	count_tokens_reading_operator(size_t *token_count, t_lexer_state *state,
 	char *str, size_t index)
 {
 	if (is_unquoted_blank(str, index) == true)
@@ -39,16 +39,16 @@ void	count_tokens_reading_operator(size_t *word_count, t_lexer_state *state,
 	else if (is_unquoted_operator_char(str, index))
 	{
 		*state = READING_OPERATOR;
-		(*word_count)++;
+		(*token_count)++;
 	}
 	else
 	{
 		*state = READING_WORD;
-		(*word_count)++;
+		(*token_count)++;
 	}
 }
 
-void	count_tokens_reading_word(size_t *word_count, t_lexer_state *state,
+void	count_tokens_reading_word(size_t *token_count, t_lexer_state *state,
 	char *str, size_t index)
 {
 	if (is_unquoted_blank(str, index) == true)
@@ -56,7 +56,7 @@ void	count_tokens_reading_word(size_t *word_count, t_lexer_state *state,
 	else if (is_unquoted_operator_char(str, index))
 	{
 		*state = READING_OPERATOR;
-		(*word_count)++;
+		(*token_count)++;
 	}
 	else
 		*state = READING_WORD;
