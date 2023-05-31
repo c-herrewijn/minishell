354258d66c728c06055cb9129ac36be03b87f973
Kevin Kroon <kkroon@student.codam.nl>
2023-05-31 17:42:44 +0200

    updated b_cd.c

--- sources/b_cd.c
+++ sources/b_cd.c
@@ -6,13 +6,44 @@
 /*   By: kkroon <kkroon@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/31 12:26:34 by kkroon        #+#    #+#                 */
-/*   Updated: 2023/05/31 12:28:47 by kkroon        ########   odam.nl         */
+/*   Updated: 2023/05/31 17:42:14 by kkroon        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//need to update the env when cd is succesfull
+//set env var PWD	
+void	update_PWD()
+{
+	
+}
+
+//if cd successful and PWD is empty or unset, make OLDPWD=  empty
+void	update_OLDPWD()
+{
+	
+}
+
+/*
+returns:
+0 : no OLDPWD, no PWD
+1 : yes OLDPWD, no PWD
+2 : no OLDPWD, yes PWD
+3 : yes OLDPWD, yes PWD
+*/
+int		pwd_in_env(t_node **head)
+{
+	int total;
+
+	total = 0;
+	if (is_in_env("export OLDPWD", head, 2) >= 0)
+		total += 1;
+	if (is_in_env("export PWD", head, 2) >= 0)
+		total += 2;
+	return total;
+}
+
+//when does cd count as succesfull, kinda depends on chdir from c
 void	b_cd(char *str, t_node **head)
 {
 	char **cd_path;
@@ -31,3 +62,4 @@ void	b_cd(char *str, t_node **head)
 		free_double_char_pointer(cd_path);
 	}
 }
+
